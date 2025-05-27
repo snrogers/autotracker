1c7604949b6b1553fc5d277e13b176fd2f28a138
Sebastian <sebastian.n.rogers@gmail.com>
2025-05-27 12:54:54 -0400

    work

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "Compiling typescript to javascript"
-npx tsc -p . --extendedDiagnostics
-rm dist/*
-echo "Bundling and minifying output"
-npx parcel build --target browser --log-level 2 --detailed-report 10 target/tracker.js
